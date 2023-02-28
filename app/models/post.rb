759ee17429bcfd1d298106eeb710d38c681f18f5
Gabriel Schiavo <gabrielschiavo0@gmail.com>
2023-02-28 11:03:19 -0300

    Cloudinary ready

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,9 @@
 class Post < ApplicationRecord
   belongs_to :user
+  # P/ uma única imagem
+  # has_one_attached :photo
+  # P/ multiplas imagens
+  has_many_attached :photos
 
   validates :content, presence: true, length: {minimum: 5}
 end
